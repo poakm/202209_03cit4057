aad99ef56b7169791b895e0a4e247d5a632b3e0d
unknown <pokam893@gmail.com>
2022-09-27 17:34:50 +0800

    lab01

--- lab01/src/19564493/lab-632D591B-2.py
+++ lab01/src/19564493/lab-632D591B-2.py
@@ -1,7 +1,13 @@
 def getDiscount( day, amount ) :
     amountAfterDiscount = 0
     # START: Write your code here
-
+    if day is "monday":
+        amountAfterDiscount = (amount / 100)*0.9
+    else:
+        if day is "tuesday":
+            amountAfterDiscount = (amount / 100)*0.95
+        else:
+            amountAfterDiscount= (amount/100) *0.99
     # END: Write you code here
     return amountAfterDiscount
 
